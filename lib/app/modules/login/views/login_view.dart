98933dd515ba41ea4e236bcaf3ee23e4a4f04c53
inidiegoooo!!!! <diegodimatheo00@gmail.com>
2021-12-09 15:52:10 +0700

    Login Fix

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -9,23 +9,48 @@ import '../controllers/login_controller.dart';
 class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Obx(() {
-          return (controller.isLoading.value)
-              ? LoadingView()
-              : ElevatedButton.icon(
-                  onPressed: () {
-                    controller.doLogin();
-                  },
-                  icon: FaIcon(FontAwesomeIcons.google),
-                  label: Text("Sign in with google"),
-                );
-        }),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 80),
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: Image.asset("assets/images/social.png"),
+            ),
+            const Text(
+              'BACAKUY',
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Yuk Bangkitkan Literasi Bangsa',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () {
+
+                },
+                icon: FaIcon(FontAwesomeIcons.google),
+                label: Text("Sign in"),
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              "Login dengan Google agar lebih praktis",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12),
+            ),
+          ],
+        ),
       ),
     );
   }
