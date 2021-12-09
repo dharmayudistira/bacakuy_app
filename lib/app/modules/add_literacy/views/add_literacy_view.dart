fb13709ee86098e37a2a6ed18dc6c39d183cd0d9
inidiegoooo!!!! <diegodimatheo00@gmail.com>
2021-12-09 16:35:04 +0700

    Halaman Add Reminder

--- lib/app/modules/add_literacy/views/add_literacy_view.dart
+++ lib/app/modules/add_literacy/views/add_literacy_view.dart
@@ -6,21 +6,201 @@ import 'package:get/get.dart';
 import '../controllers/add_literacy_controller.dart';
 
 class AddLiteracyView extends GetView<AddLiteracyController> {
+  TimeOfDay timePick = TimeOfDay.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tambah Literasi'),
+        title: Text(
+          'Bacakuy',
+        ),
         centerTitle: true,
       ),
-      body: Center(
-        child: ElevatedButton.icon(
-          onPressed: () {
-            controller.addLiteracyToDatabase();
-          },
-          icon: FaIcon(FontAwesomeIcons.plusCircle),
-          label: Text("Tambah data ke firestore"),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                children: const [
+                  SizedBox(height: 20),
+                  Text(
+                    'Add Reminder',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Read your books, 30 pages a day!',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+              Padding(
+                padding:
+                const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  children: [
+                    Stack(
+                      clipBehavior: Clip.none,
+                      alignment: Alignment.center,
+                      children: [
+                        Image.network(
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIWN2Hc90QA9GeMyJ7YTe7_Lske_gdK8RBww&usqp=CAU',
+                          width: 250,
+                          height: 250,
+                        ),
+                        Positioned(
+                          bottom: -8,
+                          right: -16,
+                          child: ElevatedButton(
+                            child: const Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(8),
+                              primary: Colors.cyan,
+                            ),
+                            onPressed: () {},
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25),
+              Column(
+                children: const [
+                  Text(
+                    "Book's Title",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  )
+                ],
+              ),
+              Column(
+                children: const [
+                  Text(
+                    "Target/Goals",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Padding(
+                    padding: EdgeInsets.only(right: 20, left: 20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+              SizedBox(height: 15),
+              Column(
+                children: const [
+                  Text(
+                    "Last Page",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Padding(
+                    padding: EdgeInsets.only(right: 120, left: 120),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(width: 120),
+                  OutlineButton(
+                      child: Icon(Icons.access_time),
+                      onPressed: () async {
+                        TimeOfDay? time = await showTimePicker(
+                          context: context,
+                          initialTime: timePick,
+                        );
+                        if (time != null) {
+                          timePick = time;
+                        }
+                      }),
+                  const SizedBox(width: 10),
+                  Text(
+                    "${timePick.hour}:${timePick.minute}",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_downward, color: Colors.white,),
+        backgroundColor: Colors.cyan,
+        onPressed: () {},
       ),
     );
   }
