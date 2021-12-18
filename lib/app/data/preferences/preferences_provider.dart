8b387b5c56b5dd8a6213f765591e6298ea787e52
inidiegoooo!!!! <diegodimatheo00@gmail.com>
2021-12-18 14:15:48 +0700

    Revisi Preferences Provider

--- lib/app/data/preferences/preferences_provider.dart
+++ lib/app/data/preferences/preferences_provider.dart
@@ -1,4 +1,5 @@
-import 'package:bacakuy_app/preferences/preferences_helper.dart';
+import 'package:bacakuy_app/app/data/preferences/preferences_helper.dart';
+import 'package:bacakuy_app/app/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
